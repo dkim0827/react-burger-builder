6f59d535393c78904ef924ac5ef5b4a7b2bc07bf
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2021-01-14 02:34:39 -0800

    added Burger Component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './burgerIngredient.css'
+
+const burgerIngredient = props => {
+    let ingredient = null;
+
+    switch(props.type){
+        case ('bread-bottom'):
+        ingredient = <div className="BreadBottom"></div>;
+        break;
+        case ('bread-top'):
+            ingredient = (
+                <div className="BreadTop">
+                    <div className="Seeds1"></div>;
+                    <div className="Seeds2"></div>;
+                </div>
+            );
+        break;
+        case ('meat'):
+            ingredient = <div className="Meat"></div>;
+            break;
+        
+    }
+};
+
+export default burgerIngredient;
