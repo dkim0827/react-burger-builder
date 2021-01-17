55b7d8a59f0fa3422f09567b13022a3b73c74c1e
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2021-01-16 21:26:18 -0800

    added default valuse for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
 
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients!</p>
+    }
     return(
         <div className='Burger'>
             <BurgerIngredient type='bread-top' />
