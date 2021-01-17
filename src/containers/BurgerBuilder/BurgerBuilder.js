497a796f9728b3e0a44142eb0695a9eed9f6682f
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2021-01-16 21:19:12 -0800

    added state for BurgerBuilder && added as props in Burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    state = {
+        ingredient: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render(){
         return(
         <Aux>
-            <div>Burger</div>
+            <Burger ingredients={this.state.ingredient}/>
             <div>Burger Controls</div>
         </Aux>
         );
